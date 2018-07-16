dc3cb492a95991f9a0d4a2a2c852b4bae4d1f5b0
arsene <arsichizy@gmail.com>
2018-07-16 17:51:23 +0200

    mlab conncetion

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-
+const MONGODB_URI = 'mongodb://arsene:arsene1@ds021979.mlab.com:21979/udemy-api';
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-module.exports = {mongoose};
+module.exports = { mongoose };
