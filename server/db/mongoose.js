2444fe72a5361c36eabd06d465598599852b7b1b
arsene <arsichizy@gmail.com>
2018-07-17 12:38:18 +0200

    Private routes auth middleware

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 const MONGODB_URI = 'mongodb://arsene:arsene1@ds021979.mlab.com:21979/udemy-api';
 const local_mongo = 'mongodb://localhost:27017/TodoApp';
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(local_mongo, { useNewUrlParser: true });
 
 module.exports = { mongoose };
