d8e6bc448303754eb55d041897bc9616575acf39
arsene <arsichizy@gmail.com>
2018-07-17 15:17:38 +0200

    logging authentication

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,27 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 20
-}
+var password = '123abc';
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var hashedPassword = '$2a$10$56QNxi77ZFPNmri0QbFKjelkKHLn9DaaWJqwfzJzw6PYEKBSSSTGm';
+bcrypt.compare('1215', hashedPassword, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 20
+// }
 
-var decoded = jwt.verify(token, '123abc');
-console.log('Decoded: ', decoded)
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('Decoded: ', decoded)
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
